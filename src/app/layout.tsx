fda5b782d21e9249f9889e2e62f65b8b5687e195
Bhavdeep Arora <bhavdeepsa@gmail.com>
2026-06-12 22:55:01 -0400

    feat: added vercel anayltics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 import { ThemeProvider } from "@/lib/theme";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: "Verso",
@@ -13,15 +15,14 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
-      {/* 1. Added background classes to <html> to ensure browser elastic scrolling bounces match the theme */}
       <html lang="en" className="bg-stone-50 dark:bg-stone-950">
         <body className="bg-stone-50 dark:bg-stone-950 text-stone-900 dark:text-stone-100 transition-colors duration-200 min-h-screen">
           <ThemeProvider>
-            {/* 2. CHANGED 'h-screen' to 'min-h-screen flex flex-col' so it can expand if content demands it */}
             <div className="min-h-screen flex flex-col">
               {children}
             </div>
           </ThemeProvider>
+        <Analytics />
         </body>
       </html>
     </ClerkProvider>
